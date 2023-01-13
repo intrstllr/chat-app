ed08fdd5d66183cc027e6a120cee7e60d3374185
intrstllr <100162882+intrstllr@users.noreply.github.com>
2023-01-13 16:40:22 +0300

    Add files via upload

--- src/components/authorization1.vue
+++ src/components/authorization1.vue
@@ -1,23 +1,23 @@
 <template>
-<div class="wrapper__form">
-        <div class="content__form">
-            <form class="authorization__form__container" @keydown.enter.prevent="">
-                <div class="div__login_blue">
-                    <input
-                        v-model="login"
-                        type="text"
-                        class="authorization__login-input_blue"
-                        placeholder="Login"
-                            />
-                </div>
-                <div class="div__password_blue">
-                    <input
-                        v-model="password"
-                        type="password"
-                        class="authorization__password-input_blue"
-                        placeholder="Password"
-                    />
-                </div>
+    <div class="wrapper__form">
+        <div class="authorization__form__container">
+            <form class="content__form" @keydown.enter.prevent="">
+                <!-- <div class="div__login_blue"> -->
+                <input
+                    v-model="login"
+                    type="text"
+                    class="authorization__login-input_blue"
+                    placeholder="Login"
+                />
+                <!-- </div> -->
+                <!-- <div class="div__password_blue"> -->
+                <input
+                    v-model="password"
+                    type="password"
+                    class="authorization__password-input_blue"
+                    placeholder="Password"
+                />
+                <!-- </div> -->
                 <div class="authorization__btns-group_blue">
                     <button
                         @click.prevent=""
@@ -35,112 +35,109 @@
                     </button>
                 </div>
             </form>
-    </div>
+        </div>
     </div>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      login: "",
-      password: "",
-      authorized1: false,
-    };
-  },
-  methods: {
-    userData() {
-      return {
-        id: 1,
-        login: "admin",
-        password: "123456",
-      };
+    data() {
+        return {
+            login: "",
+            password: "",
+            authorized1: false,
+        };
     },
-    loginVerification() {
-      const user = this.userData();
-      if (this.login === user.login && this.password === user.password) {
-        this.authorized1 = true;
-        localStorage.setItem("login1", JSON.stringify(this.authorized1));
-        this.$emit("authorized", true);
-      } else {
-        this.authorized1 = false;
-      }
-      location.reload();
+    methods: {
+        userData() {
+            return {
+                id: 1,
+                login: "admin",
+                password: "123456",
+            };
+        },
+        loginVerification() {
+            const user = this.userData();
+            if (this.login === user.login && this.password === user.password) {
+                this.authorized1 = true;
+                localStorage.setItem("login1", JSON.stringify(this.authorized1));
+                this.$emit("authorized", true);
+            } else {
+                this.authorized1 = false;
+            }
+            location.reload();
+        },
+        helpForAuthorization() {
+            alert(` login:admin \n password: 123456`);
+        },
     },
-    helpForAuthorization() {
-      alert(` login:admin \n password: 123456`);
-    },
-  },
 };
 </script>
 
 <style>
-*{
-  margin: 0px;
-  padding: 0px;
+* {
+    margin: 0px;
+    padding: 0px;
 }
 body {
-  padding: 0px;
-  margin: 0px;
-}
-
-.authorization__form__container {
-  margin: 0px auto;
-  background-color: hsl(0, 0%, 90%);
-  width: 40vh;
-  border-radius: 10px;
-  margin-left: 77vh;
-}
-.wrapper__form {
-  min-height: 100vh;
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  margin-right: 0%;
-  padding: 0px;
+    padding: 0px;
+    margin: 0px;
 }
 .content__form {
-  flex: 1 1 auto;
-  margin-top: 30vh;
-  margin-left: 0vh;
-  width: 100vh;
+    margin: 0 auto;
+    background-color: hsl(0, 0%, 90%);
+    width: 350px;
+    max-width: 400px;
+    border-radius: 10px;
+    min-width: 350px;
+    padding: 10px;
 }
-.div__login_blue {
-  padding: 30px;
-  max-width: 100vh;
-  width: 34vh;
+.wrapper__form {
+    min-height: 100vh;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    margin-right: 0%;
+    padding: 0px;
 }
-.div__password_blue {
-  padding: 30px;
-  max-width: 100vh;
-  width: 34vh;
+.authorization__form__container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 300px;
+    margin: auto;
+    max-width: 1000px;
 }
 .authorization__btns-group_blue {
-  padding: 30px;
-  max-width: 100vh;
-  width: 34vh;
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 50px;
+    display: flex;
+    justify-content: space-between;
+    padding: 30px;
 }
 .authorization__login-input_blue {
-  width: 35vh;
-  border-radius: 5px;
-  height: 30px;
+    width: 300px;
+    max-width: 300px;
+    border-radius: 5px;
+    height: 30px;
+    padding-left: 10px;
+    margin-left: 20px;
+    margin-top: 40px;
 }
 .authorization__password-input_blue {
-  width: 35vh;
-  border-radius: 5px;
-  height: 30px;
+    width: 300px;
+    max-width: 300px;
+    border-radius: 5px;
+    padding-left: 10px;
+    height: 30px;
+    margin-left: 20px;
+    margin-top: 60px;
 }
 .authorization__forgive-btn_blue {
-  height: 30px;
-  width: 50px;
-  margin-top: 0px;
+    height: 30px;
+    width: 50px;
+    cursor: pointer;
 }
 .authorization__login-btn_blue {
-  height: 30px;
-  width: 50px;
-  margin-top: 0px;
+    height: 30px;
+    width: 50px;
+    cursor: pointer;
 }
-
 </style>
--- src/pages/PageOfSecondUser.vue
+++ src/pages/PageOfSecondUser.vue
@@ -1,198 +1,239 @@
 <template>
-  <authorization2 v-if="!isAuth" @authorized="isAuth = true" />
-  <div class="visible__content" v-else>
-    <header>
-      <div class="logOut__btn" @click="logOut()">Log Out</div>
-    </header>
-    <div class="content">
-      <div class="list_body">
-        <div class="messages__List">
-          <div
-            v-for="msg in messages"
-            :key="msg.id"
-            class="mess_list"
-            :class="{ right_mess_list: msg.loginUser === `admin1` }"
-            :style="{ background: msg.color }"
-          >
-            {{ msg.body }}
-            <div class="btndel" @click.prevent="deleteMessage(msg.id)">X</div>
-            <div class="time" style="margin-top: 25px;">
-              {{ new Date(msg.timeSend).toLocaleTimeString() }}
+    <authorization2 v-if="!isAuth" @authorized="isAuth = true" />
+    <div class="visible__content" v-else>
+        <header>
+            <div class="logOut__btn" @click="logOut()">Log Out</div>
+        </header>
+        <div class="content">
+            <div class="list_body">
+                <div class="messages__List">
+                    <div
+                        v-for="msg in messages"
+                        :key="msg.id"
+                        class="mess_list"
+                        :class="{ right_mess_list: msg.loginUser === `admin2` }"
+                        :style="{ background: msg.color }"
+                    >
+                        <div class="btndel" @click.prevent="deleteMessage(msg.id)">X</div>
+                        <div class="wrapper__content__mess">
+                            <div class="mess">
+                                {{ msg.body }}
+                            </div>
+                            <div class="components__for__mess">
+                                <div class="time">
+                                    {{ new Date(msg.timeSend).toLocaleTimeString() }}
+                                </div>
+                                <div class="whoSend">
+                                    {{ msg.loginUser }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="indent"></div>
+                </div>
             </div>
-            <div class="whoSend">
-              {{ msg.loginUser }}
-            </div>
-          </div>
+            <form class="form">
+                <input type="text" class="input_for_messages" autocomplete="off" v-model="message" />
+                <button class="btnSend" @click.prevent="sendMessage">Send</button>
+            </form>
         </div>
-      </div>
-      <form class="form">
-        <input
-          type="text"
-          class="input_for_messages"
-          autocomplete="off"
-          v-model="message"
-        />
-        <button class="btnSend" @click.prevent="sendMessage">Send</button>
-      </form>
     </div>
-  </div>
 </template>
 
 <script>
 import authorization2 from "../components/authorization2.vue";
 export default {
-  components: {
-    authorization2,
-  },
-  data() {
-    return {
-      message: "",
-      messages: localStorage.getItem("messages")
-        ? JSON.parse(localStorage.getItem("messages"))
-        : [],
-      isAuth: localStorage.getItem("login2"),
-    };
-  },
-  methods: {
-    sendMessage() {
-      let msg = {};
-      msg.id = Date.now();
-      msg.body = this.message;
-      msg.timeSend = new Date();
-      msg.loginUser = "admin2";
-      msg.color = "#2ff02f";
-      this.messages.push(msg);
-      this.message = "";
-      this.saveMessages();
+    components: {
+        authorization2,
     },
-    saveMessages() {
-      let parsed = JSON.stringify(this.messages);
-      setInterval(localStorage.setItem("messages", parsed));
+    data() {
+        return {
+            message: "",
+            messages: localStorage.getItem("messages") ? JSON.parse(localStorage.getItem("messages")) : [],
+            isAuth: localStorage.getItem("login2"),
+        };
     },
-    deleteMessage(id) {
-      this.messages = this.messages.filter((it) => it.id != id);
-      this.saveMessages();
+    methods: {
+        sendMessage() {
+            let msg = {};
+            msg.id = Date.now();
+            msg.body = this.message;
+            msg.timeSend = new Date();
+            msg.loginUser = "admin2";
+            msg.color = "#2ff02f";
+            this.messages.push(msg);
+            this.message = "";
+            this.saveMessages();
+            this.scrollToBottom();
+        },
+        saveMessages() {
+            let parsed = JSON.stringify(this.messages);
+            setInterval(localStorage.setItem("messages", parsed));
+        },
+        deleteMessage(id) {
+            this.messages = this.messages.filter((it) => it.id != id);
+            this.saveMessages();
+        },
+        logOut() {
+            localStorage.removeItem("login2");
+            location.reload();
+        },
+        scrollToBottom() {
+            this.$nextTick(() => {
+                window.scrollTo({
+                    top: window.screen.height,
+                    left: 0,
+                    behavior: "smooth",
+                });
+            });
+        },
     },
-    logOut() {
-      localStorage.removeItem("login2");
-      location.reload();
+    mounted() {
+        this.scrollToBottom();
+        setInterval(() => localStorage.getItem("messages"), 1000);
     },
-  },
-  mounted() {
-    setInterval(() => localStorage.getItem("messages"), 1000);
-  },
 };
 </script>
 <style scoped>
 * {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: "Roboto", sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Roboto", sans-serif;
 }
 .visible__content {
-  width: 100vh;
-  height: 100vh;
-}
-.form {
-  position: fixed;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: rgba(0, 0, 0, 0.15);
-  padding: 5px;
-  display: flex;
-  height: 50px;
-  align-items: center;
-  backdrop-filter: blur(10px);
-}
-
-.input_for_messages {
-  height: 35px;
-  border-radius: 5px;
-  border: none;
-  margin: 5px;
-  padding: 0;
-  display: flex;
-  width: 100%;
-  outline: none;
-}
-.btnSend {
-  background: #18bd2e;
-  border: none;
-  width: 100px;
-  height: 35px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  outline: none;
-  color: white;
-  border-radius: 5px;
-}
-.messages__List {
-  list-style: none;
-  margin-top: 10px;
-  padding: 5px;
-  position: relative;
-  flex-direction: row;
-  display: flex;
-  flex-wrap: wrap;
-}
-.btndel {
-  margin-right: 0;
-  top: 4px;
-  float: right;
-  right: 4px;
-  border-radius: 50%;
-  width: 30px;
-  height: 30px;
-  background-color: #faa;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: #fff;
-  cursor: pointer;
-  position: relative;
-}
-.btndel:hover {
-  background-color: #f00;
-}
-.mess_list {
-  padding: 16px;
-  border-radius: 20px;
-  margin-top: 5px;
-  overflow-wrap: break-word;
-  width: 50vh
-}
-.logOut__btn {
-  background: #18bd2e;
-  width: 100px;
-  height: 35px;
-  border: none;
-  width: 100px;
-  height: 35px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  outline: none;
-  color: white;
-  border-radius: 5px;
-  margin-left: 1500px;
-  margin-top: 10px;
-  max-height: 35px;
+    width: 99vw;
+    height: 100vh;
 }
 .content {
-  margin-left: 50%;
-  display: flex;
-  width: 100vh;
-}
-.list_body {
-  width: 100vh;
+    width: 99vw;
+    min-height: 100vh;
 }
 header {
-  max-height: 200px;
+    max-height: 200px;
+}
+.logOut__btn {
+    background: #18bd2e;
+    width: 100px;
+    height: 35px;
+    border: none;
+    width: 100px;
+    height: 35px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    outline: none;
+    color: white;
+    border-radius: 5px;
+    margin-left: 90vw;
+    margin-top: 10px;
+    max-height: 35px;
+    cursor: pointer;
+}
+.list_body {
+    width: 51%;
+    max-width: 60%;
+    margin: auto;
+}
+.messages__List {
+    list-style: none;
+    margin-top: 10px;
+    padding: 5px;
+    position: relative;
+    flex-direction: column;
+    display: flex;
+    flex-wrap: wrap;
+    max-width: 600px;
+    width: fit-content;
+    margin: auto;
+    border: 5px;
+}
+.wrapper__content__mess {
+    padding: 16px;
+}
+.mess_list {
+    border-radius: 10px;
+    margin-top: 5px;
+    overflow-wrap: break-word;
+    width: 25vw;
+    max-width: 300px;
+    min-width: 150px;
+    display: flex;
+    flex-direction: column;
+}
+.mess {
+    margin-bottom: 15px;
 }
 .right_mess_list {
-  margin-left: 50vh;
+    margin-left: 50%;
+}
+.components__for__mess {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-end;
+    justify-content: space-between;
+}
+
+.btndel {
+    margin-right: 0;
+    top: 4px;
+    float: right;
+    right: 4px;
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
+    background-color: #faa;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    cursor: pointer;
+    margin: 5px;
+    margin-left: auto;
+}
+.btndel:hover {
+    background-color: #f00;
+}
+.form {
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.15);
+    padding: 5px;
+    display: flex;
+    height: 50px;
+    align-items: center;
+    max-width: 800px;
+    margin: auto;
+}
+.input_for_messages {
+    height: 35px;
+    border-radius: 5px;
+    border: none;
+    margin: 5px;
+    padding-left: 1%;
+    display: flex;
+    width: 100%;
+    outline: none;
+}
+.btnSend {
+    background: #18bd2e;
+    border: none;
+    width: 100px;
+    height: 35px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    outline: none;
+    color: white;
+    border-radius: 5px;
+    cursor: pointer;
+}
+.indent {
+    width: 99vh;
+    height: 60px;
+    max-width: 600px;
 }
 </style>
