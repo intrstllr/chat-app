c2f5087db57b46b8699f3ac2fbbc61204c3144c8
intrstllr <100162882+intrstllr@users.noreply.github.com>
2023-01-24 20:12:58 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <AccessViaLinks></AccessViaLinks>
+    <RouterView />
+</template>
+
+<script>
+import AccessViaLinks from "./components/AccessViaLinks.vue";
+export default {
+    components: {
+        AccessViaLinks,
+    },
+};
+</script>
+    
+<style scoped>
+* {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+    font-family: "Roboto", sans-serif;
+}
+body {
+    width: 100%;
+    margin: 0%;
+    padding: 0%;
+    height: 100%;
+}
+#app {
+    width: 100%;
+    margin-right: 0px;
+}
+</style>
---
+++ src/pages/pageOfFirstUser.vue
@@ -0,0 +1,36 @@
+<template>
+    <Authorization v-if="!authorized" @UserFirstIsLogin="complieteAuthoriz" :user="{login:'admin',password:'123456'}" />
+    <PageChat v-else @logout="logOut" :user="{id: '1'}"/>
+</template>
+
+<script>
+import PageChat from "../components/pageChat.vue";
+import Authorization from "../components/Authorization.vue";
+export default {
+    components: {
+        PageChat,
+        Authorization,
+    },
+    data() {
+        return {
+            authorized: JSON.parse(localStorage.getItem("authfirst")),
+        };
+    },
+    methods: {
+        complieteAuthoriz() {
+            this.authorized = true;
+            JSON.stringify(localStorage.setItem("authfirst", this.authorized, this.userId));
+        },
+        logOut() {
+            this.authorized = false;
+            JSON.stringify(localStorage.setItem("authfirst", this.authorized));
+        },
+    },
+};
+</script>
+
+<style>
+* {
+    font-family: "Roboto", sans-serif;
+}
+</style>
